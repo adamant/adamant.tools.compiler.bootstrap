a486f0a1046052c9d1f18b424d7eb9301c563dd9
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-07-13 12:40:03 -0400

    move lexing tests into single class to simplify

---
+++ Language.Tests/LexingTests.cs
@@ -0,0 +1,58 @@
+using Adamant.Tools.Compiler.Bootstrap.Language.Tests.Lexing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Adamant.Tools.Compiler.Bootstrap.Language.Tests
+{
+    public class LexingTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public LexingTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [MemberData(nameof(GetOneTokenSequenceData))]
+        public static void EachTokenLexes(TestTokenSequence tokens)
+        {
+            LexAssert.LexesCorrectly(tokens);
+        }
+
+        public static TheoryData<TestTokenSequence> GetOneTokenSequenceData()
+        {
+            return LexingData.GetTheoryData(LexingData.Instance.OneTokenSequences);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTwoTokenSequenceData))]
+        public static void SequenceOf2TokensLexes(TestTokenSequence tokens)
+        {
+            LexAssert.LexesCorrectly(tokens);
+        }
+
+        public static TheoryData<TestTokenSequence> GetTwoTokenSequenceData()
+        {
+            return LexingData.GetTheoryData(LexingData.Instance.TwoTokenSequences);
+        }
+
+        [Fact]
+        public void SequenceOf3TokensLexes()
+        {
+            var sequences = LexingData.Instance.ThreeTokenSequences;
+            output.WriteLine($"Sequence Count={sequences.Count:n0}");
+            foreach (var sequence in sequences)
+                LexAssert.LexesCorrectly(sequence);
+        }
+
+        [Fact]
+        public void SequenceOf4TokensLexes()
+        {
+            var sequences = LexingData.Instance.FourTokenSequences;
+            output.WriteLine($"Sequence Count={sequences.Count:n0}");
+            foreach (var sequence in sequences)
+                LexAssert.LexesCorrectly(sequence);
+        }
+    }
+}
