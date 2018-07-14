45458df4851828b1b3f26033b1077facc9847ca8
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-07-14 08:05:58 -0400

    fix fix error

--- Syntax/Parser.cs
+++ Syntax/Parser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Adamant.Tools.Compiler.Bootstrap.Core;
+using Adamant.Tools.Compiler.Bootstrap.Syntax.Tokens;
 
 namespace Adamant.Tools.Compiler.Bootstrap.Syntax
 {
