6f7d24394b1420ae55a90060718a043e2e337c9e
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-07-16 08:59:56 -0400

    more work on loading test cases

--- Language.Tests/Data/ParseTestCase.cs
+++ Language.Tests/Data/ParseTestCase.cs
@@ -1,18 +1,21 @@
 using System.Xml.Linq;
+using Adamant.Tools.Compiler.Bootstrap.Core;
 
 namespace Adamant.Tools.Compiler.Bootstrap.Language.Tests.Data
 {
     public class ParseTestCase
     {
+        public CodePath CodePath { get; }
         public string Code { get; }
         public ParseTestSyntaxKind SyntaxKind { get; }
         public XElement RootSyntax { get; }
 
-        public ParseTestCase(string code, ParseTestSyntaxKind syntaxKind, XElement rootSyntax)
+        public ParseTestCase(CodePath codePath, string code, ParseTestSyntaxKind syntaxKind, XElement rootSyntax)
         {
-            this.Code = code;
-            this.SyntaxKind = syntaxKind;
-            this.RootSyntax = rootSyntax;
+            CodePath = codePath;
+            Code = code;
+            SyntaxKind = syntaxKind;
+            RootSyntax = rootSyntax;
         }
     }
 }
--- Language.Tests/ParserTests.cs
+++ Language.Tests/ParserTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Xml.Linq;
 using Adamant.Tools.Compiler.Bootstrap.Core;
 using Adamant.Tools.Compiler.Bootstrap.Language.Tests.Data;
+using Adamant.Tools.Compiler.Bootstrap.Syntax;
+using Adamant.Tools.Compiler.Bootstrap.Syntax.Parsing;
 using Xunit;
 
 namespace Adamant.Tools.Compiler.Bootstrap.Language.Tests
@@ -14,23 +16,39 @@ namespace Adamant.Tools.Compiler.Bootstrap.Language.Tests
         [MemberData(nameof(GetAllParseTestCases))]
         public void Parses(ParseTestCase testCase)
         {
-            // TODO
+            var code = new CodeText(testCase.Code);
+            var tokens = new Lexer().Lex(code);
+            var tokenStream = new TokenStream(testCase.CodePath, code, tokens);
+            var parser = new Parser();
+            switch (testCase.SyntaxKind)
+            {
+                case ParseTestSyntaxKind.CompilationUnit:
+                    //var compilationUnit = parser.ParseCompilationUnit(tokenStream);
+                    // TODO validate syntax tree
+                    break;
+                case ParseTestSyntaxKind.Expression:
+                    throw new NotImplementedException();
+                case ParseTestSyntaxKind.Statment:
+                    throw new NotImplementedException();
+            }
         }
 
         /// Loads all *.xml test cases for parsing.
         public static TheoryData<ParseTestCase> GetAllParseTestCases()
         {
             var testCases = new TheoryData<ParseTestCase>();
-            var parseDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Parse");
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var parseDirectory = Path.Combine(currentDirectory, "Parse");
             foreach (string testFile in Directory.EnumerateFiles(parseDirectory, "*.xml", SearchOption.AllDirectories))
             {
                 var codeFile = Path.ChangeExtension(testFile, "ad");
+                var codePath = new CodePath(Path.GetRelativePath(currentDirectory, codeFile));
                 var code = File.ReadAllText(codeFile, CodeFile.Encoding);
                 var testXml = XDocument.Load(testFile).Element("test");
                 var codeXml = testXml.Element("code");
                 var syntaxKind = Enum.Parse<ParseTestSyntaxKind>(codeXml.Attribute("kind").Value.Replace("_", ""), true);
                 var rootSyntaxXml = testXml.Element("syntax").Ancestors().Single();
-                testCases.Add(new ParseTestCase(code, syntaxKind, rootSyntaxXml));
+                testCases.Add(new ParseTestCase(codePath, code, syntaxKind, rootSyntaxXml));
             }
             return testCases;
         }
