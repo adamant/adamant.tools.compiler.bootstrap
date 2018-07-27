756caf7a111befff5bf3303021f4d4579f9baabc
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-07-27 13:13:36 -0400

    testing of .net handling of lazy and tasks, notes on next steps

--- Framework.Tests/DotNetTests.cs
+++ Framework.Tests/DotNetTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Categories;
@@ -30,6 +31,34 @@ namespace Adamant.Tools.Compiler.Bootstrap.Language.Tests
         private MethodInfo GetMethodInfo(Func<int, int> f)
         {
             return f.Method;
+        }
+
+        [Fact]
+        [Exploratory]
+        public void LazyCycleThrowsInvalidOperation()
+        {
+            Lazy<int> l1 = null, l2 = null;
+            l1 = new Lazy<int>(() => l2.Value);
+            l2 = new Lazy<int>(() => l1.Value);
+            Assert.Throws<InvalidOperationException>(() => l1.Value);
+        }
+
+        [Fact]
+        [Exploratory]
+        public async Task TaskCycleDeadlocks()
+        {
+            Task<int> task = Task.FromResult(1);
+            task = GetValueAsync(() => task);
+
+            Task delayTask = Task.Delay((int)TimeSpan.FromSeconds(2).TotalMilliseconds);
+            var completedTask = await Task.WhenAny(task, delayTask);
+            Assert.Equal(delayTask, completedTask);
+        }
+
+        private async Task<int> GetValueAsync(Func<Task<int>> task)
+        {
+            await Task.Yield();
+            return await task();
         }
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,18 @@ A bootstrap compiler for the [Adamant language](http://adamant-lang.org).  That 
 ## Project Status: Alpha Active
 
 The compiler is under active development. It is in a very early stage, and there are likely issues and limitations. APIs are subject to frequent breaking changes.
+
+## Current Next Steps
+
+A rough plan of the next items to get working is:
+
+1. Checking binding mutability (`let` vs `var`) on `int` and `bool` variable types.
+2. Typecheck `int` vs `bool` expressions for basic operations.
+3. Typecheck strings to handle overload of `+`?
+4. Optional types?
+5. Add classes without members.
+6. Borrow checking on reference types.
+7. Destructors (Give a talk at the Rust meetup on drop order in Rust vs Adamant)s
+8. Interfaces
+9. Overload functions on # params
+10. Overload functions on types
