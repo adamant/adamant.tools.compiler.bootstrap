91f3bd6000e8f25b4ab73f04b64c424436a853c7
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-10-11 21:33:10 -0400

    fix failing conformance tests

---
+++ Semantics/DeclarationBuilder.cs
@@ -0,0 +1,74 @@
+using System;
+using Adamant.Tools.Compiler.Bootstrap.Core;
+using Adamant.Tools.Compiler.Bootstrap.Framework;
+using Adamant.Tools.Compiler.Bootstrap.Semantics.Names;
+using Adamant.Tools.Compiler.Bootstrap.Semantics.Nodes.Declarations;
+using Adamant.Tools.Compiler.Bootstrap.Syntax.Nodes;
+using Adamant.Tools.Compiler.Bootstrap.Syntax.Nodes.Declarations;
+using JetBrains.Annotations;
+
+namespace Adamant.Tools.Compiler.Bootstrap.Semantics
+{
+    public class DeclarationBuilder
+    {
+        public static void GatherDeclarations(
+            [NotNull] Package package,
+            [NotNull] PackageSyntax packageSyntax)
+        {
+            foreach (var compilationUnit in packageSyntax.CompilationUnits)
+            {
+                if (compilationUnit.Namespace != null)
+                    throw new NotImplementedException();
+
+                foreach (var declaration in compilationUnit.Declarations)
+                {
+                    GatherDeclarations(package, compilationUnit.CodeFile, declaration);
+                }
+            }
+        }
+
+        public static void GatherDeclarations(
+            [NotNull] Package package,
+            [NotNull] CodeFile codeFile,
+            [NotNull] DeclarationSyntax declaration)
+        {
+            switch (declaration)
+            {
+                case FunctionDeclarationSyntax function:
+                    BuildFunction(package, codeFile, function);
+                    break;
+                case ClassDeclarationSyntax @class:
+                    BuildClass(package, codeFile, @class);
+                    break;
+                default:
+                    throw NonExhaustiveMatchException.For(declaration);
+            }
+        }
+
+        private static void BuildFunction(
+            [NotNull] Package package,
+            [NotNull] CodeFile codeFile,
+            [NotNull] FunctionDeclarationSyntax function)
+        {
+            // Skip any function that doesn't have a name
+            if (function.Name?.Value is string name)
+            {
+                var fullName = new QualifiedName(name);
+                package.Add(new FunctionDeclaration(codeFile, fullName));
+            }
+        }
+
+        private static void BuildClass(
+            [NotNull] Package package,
+            [NotNull] CodeFile codeFile,
+            [NotNull] ClassDeclarationSyntax @class)
+        {
+            // Skip any function that doesn't have a name
+            if (@class.Name?.Value is string name)
+            {
+                var fullName = new QualifiedName(name);
+                package.Add(new TypeDeclaration(codeFile, fullName));
+            }
+        }
+    }
+}
--- Semantics/SemanticAnalyzer.cs
+++ Semantics/SemanticAnalyzer.cs
@@ -1,13 +1,8 @@
-using System;
 using System.Linq;
-using Adamant.Tools.Compiler.Bootstrap.Core;
 using Adamant.Tools.Compiler.Bootstrap.Core.Diagnostics;
-using Adamant.Tools.Compiler.Bootstrap.Framework;
-using Adamant.Tools.Compiler.Bootstrap.Semantics.Names;
 using Adamant.Tools.Compiler.Bootstrap.Semantics.Nodes.Declarations;
 using Adamant.Tools.Compiler.Bootstrap.Semantics.Types;
 using Adamant.Tools.Compiler.Bootstrap.Syntax.Nodes;
-using Adamant.Tools.Compiler.Bootstrap.Syntax.Nodes.Declarations;
 using JetBrains.Annotations;
 
 namespace Adamant.Tools.Compiler.Bootstrap.Semantics
@@ -18,7 +13,7 @@ namespace Adamant.Tools.Compiler.Bootstrap.Semantics
         {
             var diagnostics = new DiagnosticsBuilder();
             var package = new Package(packageSyntax.Name);
-            GatherDeclarations(package, packageSyntax);
+            DeclarationBuilder.GatherDeclarations(package, packageSyntax);
 
             // Hack for now
             foreach (var function in package.Declarations.OfType<FunctionDeclaration>())
@@ -29,45 +24,7 @@ namespace Adamant.Tools.Compiler.Bootstrap.Semantics
             return package;
         }
 
-        private static void GatherDeclarations(
-            [NotNull] Package package,
-            [NotNull] PackageSyntax packageSyntax)
-        {
-            foreach (var compilationUnit in packageSyntax.CompilationUnits)
-            {
-                if (compilationUnit.Namespace != null)
-                {
-                    throw new NotImplementedException();
-                }
-
-                foreach (var declaration in compilationUnit.Declarations)
-                {
-                    GatherDeclarations(package, compilationUnit.CodeFile, declaration);
-                }
-            }
-        }
-
-        private static void GatherDeclarations(
-            [NotNull] Package package,
-            [NotNull] CodeFile codeFile,
-            [NotNull] DeclarationSyntax declaration)
-        {
-            switch (declaration)
-            {
-                case FunctionDeclarationSyntax function:
-                    // Skip any function that doesn't have a name
-                    if (function.Name?.Value is string name)
-                    {
-                        var fullName = new QualifiedName(name);
-                        package.Add(new FunctionDeclaration(codeFile, fullName));
-                    }
-                    break;
-                default:
-                    throw NonExhaustiveMatchException.For(declaration);
-            }
-        }
-
-        private static void DetermineEntryPoint(Package package, DiagnosticsBuilder diagnostics)
+        private static void DetermineEntryPoint([NotNull] Package package, [NotNull] DiagnosticsBuilder diagnostics)
         {
             var mainFunctions = package.Declarations.OfType<FunctionDeclaration>()
                 // TODO make an easy way to construct and compare qualified names
