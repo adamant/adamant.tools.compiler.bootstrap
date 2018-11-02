3ebeb53ad21436620f8c3a1ca68ced548c721e5c
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-11-02 11:59:57 -0400

    write example in runtime library of calling windows system allocate functions

--- Emit.C/RuntimeLibrary.c
+++ Emit.C/RuntimeLibrary.c
@@ -1,1 +1,22 @@
 ﻿#include "RuntimeLibrary.h"
+//#include <windows.h>
+
+typedef void* LPVOID;
+typedef LPVOID HANDLE;
+typedef size_t SIZE_T;
+typedef uint32_t DWORD;
+typedef int32_t BOOL;
+
+HANDLE GetProcessHeap();
+LPVOID HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
+LPVOID HeapReAlloc(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem, SIZE_T dwBytes);
+BOOL HeapFree(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem);
+DWORD GetLastError();
+
+void test()
+{
+	void* ptr = HeapAlloc(GetProcessHeap(), 0, 10);
+	HeapFree(GetProcessHeap(), 0, ptr);
+    //void* ptr = aligned_alloc(8, 32);
+    //free(ptr);
+}
