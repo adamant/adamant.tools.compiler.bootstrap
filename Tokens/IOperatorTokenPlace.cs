4c1d9bea6e3e2b1395832f6f17fda575b79e0f1a
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2018-11-09 15:00:28 -0500

    fix issue with IMissingToken

--- Tokens/IOperatorTokenPlace.cs
+++ Tokens/IOperatorTokenPlace.cs
@@ -1,4 +1,6 @@
 namespace Adamant.Tools.Compiler.Bootstrap.Tokens
 {
     public interface IOperatorTokenPlace : ITokenPlace { }
+
+    public partial interface IMissingToken : IOperatorTokenPlace { }
 }
---
+++ Tokens/IOverloadableOperatorTokenPlace.cs
@@ -0,0 +1,4 @@
+namespace Adamant.Tools.Compiler.Bootstrap.Tokens
+{
+    public interface IOverloadableOperatorTokenPlace : ITokenPlace { }
+}
