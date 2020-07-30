8b16ad49c60371d9b39cdad7babefd51e91bf10a
Jeff Walker <Jeff@WalkerCodeRanger.com>
2020-07-29 21:25:25 -0700

    SymbolTree placeholder classes

---
+++ Symbols/SymbolTree.cs
@@ -0,0 +1,10 @@
+namespace Adamant.Tools.Compiler.Bootstrap.Symbols
+{
+    /// <summary>
+    /// A symbol tree is a immutable collection of symbols that answers the question:
+    /// For any given symbol, what are its child symbols.
+    /// </summary>
+    public class SymbolTree
+    {
+    }
+}
---
+++ Symbols/SymbolTreeBuilder.cs
@@ -0,0 +1,9 @@
+namespace Adamant.Tools.Compiler.Bootstrap.Symbols
+{
+    /// <summary>
+    /// Builder for a <see cref="SymbolTree"/>
+    /// </summary>
+    public class SymbolTreeBuilder
+    {
+    }
+}
